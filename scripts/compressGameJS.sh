6c64d004aeceeb9f12efc6bfd8e69974999e8847
garfield <garfield@users.noreply.github.com>
2024-04-18 19:59:18 +0800

    sharpshooter create project structure

---
+++ scripts/compressGameJS.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+JS_PATH="/Users/hangzhang/Dev/jiafie7/sharpshooter/game/static/js/"
+JS_PATH_DIST="${JS_PATH}dist/"
+JS_PATH_SRC="${JS_PATH}src/"
+
+find ${JS_PATH_SRC} -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
+
