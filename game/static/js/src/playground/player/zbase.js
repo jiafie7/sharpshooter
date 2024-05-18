2fe0658bac264aabe152391fbe38e6e3f08b14f0
garfield <garfield@users.noreply.github.com>
2024-05-18 20:27:51 +0800

    multimode chat system

--- game/static/js/src/playground/player/zbase.js
+++ game/static/js/src/playground/player/zbase.js
@@ -78,7 +78,7 @@ class Player extends AcGameObject {
       return false;
     });
     this.playground.game_map.$canvas.mousedown(function (e) {
-      if (outer.playground.state !== "fighting") return false;
+      if (outer.playground.state !== "fighting") return true;
 
       const rect = outer.ctx.canvas.getBoundingClientRect(); // for acapp
       //console.log(rect.left);
@@ -121,6 +121,17 @@ class Player extends AcGameObject {
     });
 
     this.playground.game_map.$canvas.keydown(function (e) {
+      if (e.which === 13) {
+        if (outer.playground.mode === "multi mode") {
+          outer.playground.chat_field.show_input();
+          return false;
+        }
+      } else if (e.which === 27) {
+        if (outer.playground.mode === "multi mode") {
+          outer.playground.chat_field.hide_input();
+        }
+      }
+
       if (outer.playground.state !== "fighting") return true;
 
       if (e.which === 81) {
